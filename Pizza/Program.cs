bc0a76d34d926d25fff439c77d23c7909e991fd9
Benjamin Nesom <17NesomB@fulford.york.sch.uk>
2022-09-23 15:19:06 +0100

    Added a selection for enterring pizza

--- Pizza/Program.cs
+++ Pizza/Program.cs
@@ -10,6 +10,37 @@ namespace Pizza
     {
         static void Main(string[] args)
         {
+            List<string> toppings = new List<string> {
+                "Pepperoni", "Margerita", "Hawaiian"
+            };
+
+
+            string selection = ChooseFrom(toppings);
+            Console.WriteLine($"You've chosen {selection}");
         }
+
+        private static string ChooseFrom(List<string> options)
+        {
+            bool valid = false;
+            string choice = "";
+
+            Console.WriteLine("What pizza would you like we have: ");
+            for (int i = 0; i < options.Count; i ++)
+            {
+                Console.WriteLine($"{i+1} : {options[i]}");
+
+            }
+
+            while (valid == false)
+            {
+                Console.WriteLine($"Enter a number from 1 - {options.Count}");
+                choice = Console.ReadLine();
+                
+                
+            }
+            int intChoice = int.Parse(choice);
+            return (options[intChoice - 1]);
+        }
+
     }
 }
